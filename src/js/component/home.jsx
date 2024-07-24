15e2df76c00ed8bbc44483ea68c4f302b93cdd95
Angel Cuevas <163451592+AngelCS94@users.noreply.github.com>
2024-07-24 21:08:46 +0000

    cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -65,9 +65,7 @@ const Home = () => {
             },
             body: JSON.stringify(newTodos),
         })
-            .then((response) => {
-                if (!response.ok) throw new Error('Error al eliminar el todo');
-                return response.json();
+            .then((response) => {response.json();
             })
             .then(() => {
                 getTodos();
